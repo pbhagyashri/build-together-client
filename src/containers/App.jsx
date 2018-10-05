a4c1450dff07ebe3430808e9976f7f4d415a8ea4
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-10-05 13:54:50 -0400

    Add containers and componenets derectories in src directory add app.jsx and app.css to containers directory

---
+++ src/containers/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Add Header Here</h1>
+        <h1>Add Home Here</h1>
+        <h1>Add Footer Here</h1>
+      </section>
+    );
+  }
+}
+
+export default App;

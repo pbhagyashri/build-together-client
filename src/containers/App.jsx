2b918a5879661c0fab7c54adc10f1e4342562232
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-10-05 22:29:14 -0400

    Add variables partial. add google fonts to App.scss. import variables from variable partial

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
 
 class App extends Component {
   render() {
     return (
       <section>
-        <h1>Add Header Here</h1>
+        <h1>Build Together</h1>
         <h1>Add Home Here</h1>
         <h1>Add Footer Here</h1>
       </section>

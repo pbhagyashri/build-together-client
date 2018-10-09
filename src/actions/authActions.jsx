607aa05844ddb7c1b7a304ccaee6a4f3a1e0ad01
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-10-09 14:00:56 -0400

    change fetch url

--- src/actions/authActions.jsx
+++ src/actions/authActions.jsx
@@ -1,4 +1,5 @@
-const API_URL = "http://192.168.1.190:3001/api"
+//const API_URL = "http://192.168.1.190:3001/api"
+const API_URL = "http://build-together-server.herokuapp.com/api"
 
 const loginSuccess = (user) => {
   return {
